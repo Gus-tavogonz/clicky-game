cd64528438aa42d9027c3b9ad5e901b833a69a27
unknown <gustavogonz3393@gmail.com>
2018-10-27 11:24:37 -0500

    Updates

---
+++ precache-manifest.b30550c50af9a827202840ac6c5a2674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "d6c5d92181c76222d03d",
+    "url": "/clicky-game/static/js/main.d6c5d921.chunk.js"
+  },
+  {
+    "revision": "3a58f11f13f5574bd1fa",
+    "url": "/clicky-game/static/js/1.3a58f11f.chunk.js"
+  },
+  {
+    "revision": "d6c5d92181c76222d03d",
+    "url": "/clicky-game/static/css/main.9b047a39.chunk.css"
+  },
+  {
+    "revision": "d2d2ed574ad21aac79e389e43a03902b",
+    "url": "/clicky-game/index.html"
+  }
+];
